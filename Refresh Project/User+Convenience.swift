0d0b3544b431a9badb1602c25f96575e0565f4dc
Hector L <hectoremil21@hotmail.com>
2020-04-23 16:54:13 -0400

    Create User+Convenience.swift

---
+++ Refresh Project/User+Convenience.swift
@@ -0,0 +1,12 @@
+//
+//  User+Convenience.swift
+//  Refresh Project
+//
+//  Created by Karen Rodriguez on 4/23/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
