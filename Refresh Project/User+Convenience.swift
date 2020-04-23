a1fc831a8820865c534b47c7f5bf6ec8f90016aa
Hector L <hectoremil21@hotmail.com>
2020-04-23 17:22:33 -0400

    Implement initializer in Usaer+Convenience

--- Refresh Project/User+Convenience.swift
+++ Refresh Project/User+Convenience.swift
@@ -13,4 +13,16 @@ import CoreData
 // I had to restart this xcodeproject for it to find User. Something to keep in mind.
 extension User {
     
+    // MARK: - What is 'convenience' init
+    // MARK: - Why do we use @discardableResult
+    
+    @discardableResult convenience init(username: String, password: String, email: String, id: UUID = UUID(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        // Don't think of this as some sort of class. We're literally just making it easier for us, but we're still using CoreData here. It needs to know what context it will be created in.
+        self.init(context: context)
+        
+        self.username = username
+        self.password = password
+        self.email = email
+    }
+    
 }
